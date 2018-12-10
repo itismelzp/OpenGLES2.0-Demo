4a45079eb7be595c9a9c2ce53378d4a3b49a115b
lizhiping03 <lizhiping03@baidu.com>
2018-12-10 10:57:56 +0800

    some setting

---
+++ app/src/main/java/com/lzp/helloopengles20/HelloOpenGLES20.java
@@ -0,0 +1,53 @@
+package com.lzp.helloopengles20;
+
+import android.app.Activity;
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+
+public class HelloOpenGLES20 extends Activity {
+
+    private GLSurfaceView mGLView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Create a GLSurfaceView instance and set it
+        // as the ContentView for this Activity
+        mGLView = new HelloOpenGLES20SurfaceView(this);
+        setContentView(mGLView);
+        getWindow().setBackgroundDrawable(null);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // The following call pauses the rendering thread.
+        // If your OpenGL application is memory intensive,
+        // you should consider de-allocating objects that
+        // consume significant memory here.
+        mGLView.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // The following call resumes a paused rendering thread.
+        // If you de-allocated graphic objects for onPause()
+        // this is a good place to re-allocate them.
+        mGLView.onResume();
+    }
+
+    class HelloOpenGLES20SurfaceView extends GLSurfaceView {
+
+        public HelloOpenGLES20SurfaceView(Context context){
+            super(context);
+
+            // Create an OpenGL ES 2.0 context.
+            setEGLContextClientVersion(2);
+            // Set the Renderer for drawing on the GLSurfaceView
+            setRenderer(new HelloOpenGLES20Renderer());
+        }
+    }
+}
